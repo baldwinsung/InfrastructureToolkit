0afd1470cd3115bb89dda6d2e08a17859b372d74
Baldwin Sung <baldwinsung@me.com>
2018-01-18 03:34:41 -0500

    Pull list of droplets from digital ocean

---
+++ list-droplets.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import requests, json
+
+accessToken = "TOKEN"
+url         = "https://api.digitalocean.com/v2/droplets"
+headers     = {"Authorization": "Bearer "+accessToken}
+
+r           = requests.get(url, headers=headers)
+
+return_code = str(r.status_code)
+#print ( return_code )
+results     = r.json()
+#print (json.dumps(results, indent=2))
+
+for machine in results['droplets']:
+    hostname  = machine['name']
+    ipaddress = machine['networks']['v4'][0]['ip_address']
+
+    print hostname, ipaddress
